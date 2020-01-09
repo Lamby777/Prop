8d056d41a4ac34311b7f81f3c7fc5c69fb61761a
Lamby777 <sheepareawesome777@gmail.com>
2020-01-09 19:10:41 +0000

    Canvas

---
+++ js/sketch.js
@@ -0,0 +1,10 @@
+/////////////////////////////////
+//   Prop 3 by DexieTheSheep   //
+//   Prop 3 by DexieTheSheep   //
+/////////////////////////////////
+
+var canvas = document.getElementById("prop-canvas");
+window.onload = window.onresize = function() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
