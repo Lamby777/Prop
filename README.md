5858720c5d7dad4eb4a86e939d6a396830575412
Lamby777 <sheepareawesome777@gmail.com>
2020-01-10 14:44:49 +0000

    update to canvas mechanics

--- README.md
+++ README.md
@@ -5,4 +5,10 @@ can be used to create Games, Art Tools, Tests and more.
 
 ## Getting Started
 
-Download the repo with the green button
+Download the repo with the green button, and add your own
+JS to make a game! You can find extensive documentation
+[here](https://dev.prop3.lamby777.repl.co).
+
+## Stat Variables
+
+frames
--- js/sketch.js
+++ js/sketch.js
@@ -3,8 +3,103 @@
 //   https://github.com/Lamby777/Prop    //
 ///////////////////////////////////////////
 
-var canvas = document.getElementById("prop-canvas");
+// Constants
+const DEV = true; // Shows stats 4 nerds
+const SHOW_FPS = true;
+
+game = {
+	ground: {
+		height: 140
+	},
+}
+
+level = {
+	friction: 0.8,
+	accel: 1.9,
+	gravity: 1,
+	maxfall: 10
+}
+
+// Initialize Variables
+let lastFramecount, fps, cx, cy;
+
+// Canvas
+let canvas = document.getElementById("prop-canvas");
+const c = canvas.getContext("2d");
+
+// On page load, plus every time the page is resized
 window.onload = window.onresize = function() {
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
+	cx = canvas.width;
+	cy = canvas.height;
 }
+
+class Prop {
+	constructor(x, y, w, h) {
+		this.x = x,
+		this.y = y,
+		this.w = w,
+		this.h = h;
+	}
+	function touching(rect) {
+		return !(rect.x > (this.x + this.w) ||
+				(rect.x + rect.w) < this.x ||
+				rect.y > (this.y + this.h) ||
+				(rect.y + rect.h) < this.y);
+	}
+	function image(src) {
+		//
+	}
+	function prepareUpdate() {
+			if (this.meta.physics) {
+				this.y += gravity - yvel;
+			}
+			if (this.sheet) this.animate();
+			this.x += this.xvel;
+			this.y -= this.yvel;
+			if (!this.canBypassCanvasBorder) {
+				if (this.x + this.width > gameCanvas.canvas.width) {
+					this.x = gameCanvas.canvas.width - this.width;
+				}
+				if (this.x < 0) {
+					this.x = 0;
+				}
+			}
+			if (this.meta.screenWrap) {
+				if (this.onRightEdge()) {
+					this.x = 1;
+				} else if (this.onLeftEdge()) {
+					this.x = (gameCanvas.canvas.width - this.width) - 1;
+				}
+			}
+		}
+}
+
+function update() {
+	clear();
+	redrawSettings();
+	redrawAmbient();
+	redrawProps();
+	if (SHOW_FPS) redrawFramecount();
+}
+
+function redrawProps() {
+	for (prop of props) {}
+}
+
+function redrawFramecount() {
+	if(!lastFramecount) {
+		lastFramecount = performance.now();
+		fps = 0;
+		return;
+	}
+	let delta = (performance.now() - lastFramecount)/1000;
+	lastFramecount = performance.now();
+	fps = 1/delta;
+	c.strokeColor = white;
+	c.fillColor = black;
+	c.strokeText(fps.toString(), cx-24, cy-24);
+}
+
+function redrawSettings() {}
